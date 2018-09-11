8bc27edef81a7d3fd6f0a4850bfbcfd60af40ec3
Lance Hardy <lance.hardy@getg5.com>
2018-09-11 14:29:03 -0700

    an approach that may make sense, migration-script-v2.rb

--- migration-script-v2.rb
+++ migration-script-v2.rb
@@ -1,3 +1,5 @@
+# 1) PULL FROM CLS
+
 # get configurable attributes from one cls at a time
 app = "g5-cls-ix846vtx-storbox-self-s"
 
@@ -18,67 +20,34 @@ configs_array = []
 # gather all configurable attributes which includes custom and default values. It should also include Lead Task 'enabled' info if added to api
   json_output.parsed_response['configurable_attributes'].map do |obj| configs_array << obj end
 
-# connect to email service - tbd
+# 2) DETERMINE DATA FORMAT REQUIRED
 
-# the goal is to
-## 1) select client
-## 2) select client locations
-## 3) delete all configurable attributes associated with those locations
-## 4) replace all configurable attributes with json output from api
+# the way data is entered in email-service it makes sense to take the json output and separate it into individual location data collections, eg
+# outputs would be comma separated strings, acceptable for form inputs in email service
+# this illustrates and assumption of two possible values in one input but it should be determined what the max number of values may be
+{
+  location_urn1: { to_emails: 'asdf,asdf', to_names: 'asdf,asdf', from_email: 'asdf,asdf', reply_to_email: 'asdf, asdf'....}
+  location_urn2: { to_emails: 'asdf,asdf', to_names: 'asdf,asdf', from_email: 'asdf,asdf', reply_to_email: 'asdf, asdf'....}
+  ...
+}
 
-# Matt Bohme is tasked with deleting client data from email service
-# should the delete and insert task be syncrhonous
+# 3) CHECK ENV VAR TO SEE IF G5_EMAILS ALREADY SET, ELSE RETURN
 
-# overrides_controller contains both delete and create functions - use as guideline
-# there is no api currently which displays overrides
+# in case the client is already using email service, check heroku env var first
 
-def destroy
-  @override_target.destroy
-  flash[:notice] = 'Successfully deleted Override'
-  respond_with @override_target, location: overrides_path
-end
+# 4) CONNECT TO EMAIL SERVICE - THE DELETE AND INSERT TASK CAN BE SYNCHRONOUS
 
-def update
-  if @override_target.update_attributes(override_target_params)
-    flash[:notice] = 'Successfully updated Override'
-  else
-    @override_target.set_all_overrides
-  end
-  respond_with @override_target, location: overrides_path(client_urn: client_urn)
-end
+# wrap functionality in transaction so no database commit will happen if something goes wrong
+# START
 
-def new
-  @override_target.set_all_overrides
-end
+# 5) FIND ALL OVERRIDES, OVERRIDE_TARGETS, RECIPIENTS, RECIPIENT_TARGETS, DISABLES AND DELETE
 
-def create
-  @override_target = OverrideTarget.new(override_target_params)
-  if @override_target.save
-    flash[:notice] = 'Successfully created Override'
-  else
-    @override_target.set_all_overrides
-  end
-  respond_with @override_target, location: overrides_path(client_urn: client_urn)
-end
+# example, loop through recipient targets where client urn is whatever, destroy
+# check for default override flags and do not delete defaults. eg, Recipient has 'Set in g5-hub' flag
 
-private
-def load_target
-  @override_target = OverrideTarget.find(params[:override_target_id])
-end
+# 6) REBUILD FROM CLS API JSON (OR REFORMATTED DATA)
 
-def model_client_urn
-  @override_target.try(:client_urn)
-end
+# example, loop through recipient targets where client urn is whatever, insert
+# check for default override flags and do not replace defaults. eg, Recipient has 'Set in g5-hub' flag
 
-def override_target_params
-  return {} unless params[:override_target].present?
-  params.require(:override_target).permit(:id, :client_urn, :location_urn, :cls_form_type_id, :email_type_id, overrides_attributes: [:id, :field, :value])
-end
-
-def load_new_target
-  @override_target = if params[:clone_override_target_id]
-                       OverrideTarget.find(params[:clone_override_target_id]).clone_override_target
-                     else
-                       OverrideTarget.new(client_urn: client_urn)
-                     end
-end
+# END 
